9385d542ef2dd9173ae8b220d26dc9c17a3e4ade
Pulkit Aggarwal <pulkit.aggarwal@nagarro.com>
2022-08-01 09:57:55 +0530

    Add @RepositoryRestResource bcs of which no need to add controller

--- src/main/java/com/pulkit/springbootjpa/dao/AlienDao.java
+++ src/main/java/com/pulkit/springbootjpa/dao/AlienDao.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.pulkit.springbootjpa.model.Alien;
 
 // CrudRepository contains all CRUD method ( return iterable)
 // JpaRepository return list and It has pagination+sorting+ extends CrudRepository
+
+//@RepositoryRestResource(collectionResourceRel = "aliens", path = "aliens") - no need to specify controller it will manage by itself
 public interface AlienDao extends JpaRepository<Alien, Integer> {
 	List<Alien> findByTech(String tech); // used naming convention
 	
